8ee627d52fdf90b6d23d3255b8316b6669874820
Malek Mousa <malek.mousa1407@gmail.com>
2021-09-01 17:44:37 +0400

    Added Socket.io.

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 require('dotenv').config();
-const app = express();
+const http = require('http');
 const path = require('path');
+const socketio = require('socket.io');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 
 // Set static folder
 app.use(express.static(path.join(__dirname,'public')));
 
-
+io.on('connection', () =>{
+    console.log('New WS connection...');
+});
 
 const PORT = 5000 || process.env.PORT;
-app.listen(PORT,() => 
+server.listen(PORT,() => 
     console.log(`Server is running on http://localhost:${PORT}`)
 );
