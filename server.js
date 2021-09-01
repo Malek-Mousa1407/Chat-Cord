2897fd29805e7e873467e5390a988d459be8c66a
Malek Mousa <malek.mousa1407@gmail.com>
2021-09-01 18:07:19 +0400

    Added Basic Functionality To User Connections.

--- server.js
+++ server.js
@@ -13,8 +13,18 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname,'public')));
 
-io.on('connection', () =>{
-    console.log('New WS connection...');
+// Runs when client connects
+io.on('connection', socket =>{
+    // Welcome current user
+    socket.emit('message','Welcome To Chat Cord!');
+    
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    // Runs when client disconnects
+    socket.on('disconnect', () =>{
+        io.emit('message', 'A user has left the chat')
+    });
 });
 
 const PORT = 5000 || process.env.PORT;
